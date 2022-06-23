e3324b2b67f16c2a289391cb44c590ec735c903f
mano-pihema <mano-pihema-laptop@LAPTOP-MNKDSN6R.localdomain>
2022-06-23 21:50:01 +1200

    answer buttons added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { getquizApi } from '../apis/quizApi'
 
@@ -10,6 +9,7 @@ function App() {
   const [data, setData] = useState([])
 
   function handleClick(event) {
+    console.log(event.target)
     event.target
   }
 
@@ -17,27 +17,40 @@ function App() {
     // const apiData = getquizApi()
     // console.log(apiData)
     getquizApi()
-    .then((res) => {
-      console.log('this is front end data ', res)
-      setData(res)
-    })
-    .catch((err) => {
-      console.error(err.message)
-    })
+      .then((res) => {
+        console.log('this is front end data ', res)
+        setData(res)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
   }, [])
 
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <h1>Questions</h1>
         <ul>
           {data.map((quiz) => (
-            <li key={quiz.id}>{quiz.question}</li>
+            <>
+              <li key={quiz.id}>{quiz.question}</li>
+
+              <li>
+                {quiz.answers.map((ans) => (
+                  <button key={ans} onClick={handleClick}>
+                    {ans}
+                  </button>
+                ))}
+              </li>
+            </>
           ))}
         </ul>
+        {/* {data.map((ans) => (
+          <button key={ans.id} onClick={handleClick}>
+            {ans.answers}
+          </button>
+        ))} */}
       </div>
-
-      <form>{}</form>
     </>
   )
 }
