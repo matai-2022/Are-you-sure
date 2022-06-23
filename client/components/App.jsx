6d3f4fe8adeed80949d328dc0ea1c6321e546597
saia-maka <makasiosaia@gmail.com>
2022-06-23 22:00:04 +1200

    prototype

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+// import { set } from '../../server/server'
 
 import { getquizApi } from '../apis/quizApi'
 
@@ -8,33 +8,53 @@ function App() {
 
   const [question, setQuestion] = useState('')
   const [data, setData] = useState([])
+  const [answer, setAnswer] = useState('')
 
   function handleClick(event) {
     event.target
   }
 
+
   useEffect(() => {
     // const apiData = getquizApi()
     // console.log(apiData)
     getquizApi()
-    .then((res) => {
-      console.log('this is front end data ', res)
-      setData(res)
-    })
-    .catch((err) => {
-      console.error(err.message)
-    })
+      .then((res) => {
+        console.log('this is front end data ', res)
+        setData(res)
+      })
+      .finally(() => {
+        console.log(answer, ' finally')
+        // setAnswer(res.correctAnswer)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
   }, [])
+
+  function selectHandler(e) {
+    console.log(e.target.value)
+  }
 
   return (
     <>
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
+        {/* <ul> */}
           {data.map((quiz) => (
-            <li key={quiz.id}>{quiz.question}</li>
+            <>
+              <h3 key={quiz.id}>{quiz.question}</h3>
+              {quiz.answers.sort().map((answer) => {
+                // setAnswer(answer)
+                return <button key={answer} value={answer} onClick={selectHandler}>
+                  {answer}
+                </button>
+                // return <p key={answer}>- {answer}</p>
+              })}
+              <p>(correct answer: {quiz.correctAnswer})</p>
+            </>
           ))}
-        </ul>
+        {/* </ul> */}
       </div>
 
       <form>{}</form>
