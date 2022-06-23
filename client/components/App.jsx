440e61cdaee1f198dd05f6cc62f5d954558cd9b7
saia-maka <makasiosaia@gmail.com>
2022-06-23 23:15:20 +1200

    create quiz functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
-// import { set } from '../../server/server'
 
 import { getquizApi, fetchAnswer } from '../apis/quizApi'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [turnsCount, setTurnsCount] = useState(0)
+  const [quizCount, setQuizCount] = useState(0)
   const [question, setQuestion] = useState('')
   const [data, setData] = useState([])
   const [answer, setAnswer] = useState('')
@@ -14,26 +13,38 @@ function App() {
     event.target
   }
 
-
   useEffect(() => {
     // const apiData = getquizApi()
     // console.log(apiData)
     getquizApi()
       .then((res) => {
-        console.log('this is front end data ', res)
+        // console.log(console.log(turnsCount))
+        console.log('turns: ', turnsCount)
+        console.log('correct: ', quizCount)
         setData(res)
       })
       .catch((err) => {
         console.error(err.message)
       })
-  }, [])
+  }, [turnsCount])
 
+  // useEffect(() => {
+  //   incrementCount()
+  // console.log('correct: ', quizCount)
+  // }, [turnsCount])
+
+  function incrementCount() {
+    setQuizCount(quizCount + 1)
+  }
 
   function selectHandler(e) {
     if (e.target.value == e.target.name) {
       console.log('CORRECT!')
+      incrementCount()
+      setTurnsCount(turnsCount + 1)
     } else {
-      console.log('ARE YOU SURE?')
+      console.log('ARE YOU SURE!')
+      setTurnsCount(turnsCount + 1)
     }
   }
 
@@ -47,19 +58,26 @@ function App() {
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
         {/* <ul> */}
-          {data.map((quiz) => (
-            <>
-              <h3 key={quiz.id}>{quiz.question}</h3>
-              {quiz.answers.sort().map((answer) => {
-                // setAnswer(answer)
-                return <button key={answer} value={answer} name={quiz.correctAnswer} onClick={selectHandler}>
+        {data.map((quiz) => (
+          <>
+            <h3 key={quiz.id}>{quiz.question}</h3>
+            {quiz.answers.sort().map((answer) => {
+              // setAnswer(answer)
+              return (
+                <button
+                  key={answer}
+                  value={answer}
+                  name={quiz.correctAnswer}
+                  onClick={selectHandler}
+                >
                   {answer}
                 </button>
-                // return <p key={answer}>- {answer}</p>
-              })}
-              {/* <p>(correct answer: {quiz.correctAnswer})</p> */}
-            </>
-          ))}
+              )
+              // return <p key={answer}>- {answer}</p>
+            })}
+            <p>(correct answer: {quiz.correctAnswer})</p>
+          </>
+        ))}
         {/* </ul> */}
       </div>
 
