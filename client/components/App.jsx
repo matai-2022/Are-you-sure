45a1b45b48ca07d3f6682ab45f4304b50f9784a7
saia-maka <makasiosaia@gmail.com>
2022-06-23 23:52:19 +1200

    create results functionality & complete mvp

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { getquizApi, fetchAnswer } from '../apis/quizApi'
 
 function App() {
   const [turnsCount, setTurnsCount] = useState(0)
   const [quizCount, setQuizCount] = useState(0)
-  const [question, setQuestion] = useState('')
+  const [gameIsOver, setGameIsOver] = useState(false)
   const [data, setData] = useState([])
-  const [answer, setAnswer] = useState('')
-
-  function handleClick(event) {
-    event.target
-  }
+  const navigate = useNavigate()
 
   useEffect(() => {
     // const apiData = getquizApi()
     // console.log(apiData)
+    goToResults()
     getquizApi()
       .then((res) => {
         // console.log(console.log(turnsCount))
         console.log('turns: ', turnsCount)
         console.log('correct: ', quizCount)
+        console.log('game over? ', gameIsOver)
+
         setData(res)
       })
       .catch((err) => {
@@ -28,57 +28,80 @@ function App() {
       })
   }, [turnsCount])
 
-  // useEffect(() => {
-  //   incrementCount()
-  // console.log('correct: ', quizCount)
-  // }, [turnsCount])
-
   function incrementCount() {
     setQuizCount(quizCount + 1)
+  }
+
+  function goToResults() {
+    if (turnsCount == 5) {
+      setGameIsOver(true)
+    } else {
+      return
+    }
   }
 
   function selectHandler(e) {
     if (e.target.value == e.target.name) {
       console.log('CORRECT!')
+      console.log('game over? ', gameIsOver)
       incrementCount()
-      setTurnsCount(turnsCount + 1)
+      setTimeout(()=>{
+        setTurnsCount(turnsCount + 1)
+      },1000)
     } else {
       console.log('ARE YOU SURE!')
-      setTurnsCount(turnsCount + 1)
+      // setTurnsCount(turnsCount + 1)
+      setTimeout(()=>{
+        setTurnsCount(turnsCount + 1)
+      },600)
     }
   }
 
-  // function answerCollector(e) {
-  //   console.log(data.correctAnswer, ' this is acollector')
-  //   setAnswer(data.correctAnswer)
+  // function resetHandler() {
+  //   navigate('/')
   // }
+
+  const game = (
+    <>
+      {data.map((quiz) => (
+        <>
+          <h3 key={quiz.id}>{quiz.question}</h3>
+          {quiz.answers.sort().map((answer) => {
+            // setAnswer(answer)
+            return (
+              <button
+                key={answer}
+                value={answer}
+                name={quiz.correctAnswer}
+                onClick={selectHandler}
+              >
+                {answer}
+              </button>
+            )
+            // return <p key={answer}>- {answer}</p>
+          })}
+          <p>(correct answer: {quiz.correctAnswer})</p>
+        </>
+      ))}
+    </>
+  )
+
+  const gameover = (
+    <>
+      <h1>GAMEOVER!!</h1>
+      <h1>You scored {quizCount}/{turnsCount}!</h1>
+      {/* <button onClick={resetHandler}>
+        Play again?
+      </button> */}
+      <a href="/">Play again?</a>
+    </>
+  )
 
   return (
     <>
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
-        {/* <ul> */}
-        {data.map((quiz) => (
-          <>
-            <h3 key={quiz.id}>{quiz.question}</h3>
-            {quiz.answers.sort().map((answer) => {
-              // setAnswer(answer)
-              return (
-                <button
-                  key={answer}
-                  value={answer}
-                  name={quiz.correctAnswer}
-                  onClick={selectHandler}
-                >
-                  {answer}
-                </button>
-              )
-              // return <p key={answer}>- {answer}</p>
-            })}
-            <p>(correct answer: {quiz.correctAnswer})</p>
-          </>
-        ))}
-        {/* </ul> */}
+        {gameIsOver ? gameover : game}
       </div>
 
       <form>{}</form>
