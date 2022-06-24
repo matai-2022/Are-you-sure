f440f06ba1bc11b534608e614a13e024cad05be9
mano-pihema <mano-pihema-laptop@LAPTOP-MNKDSN6R.localdomain>
2022-06-24 13:29:48 +1200

    timer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react'
 
 import { getquizApi } from '../apis/quizApi'
-
+import Timer from './Timer'
 function App() {
   const [turnsCount, setTurnsCount] = useState(0)
   const [quizCount, setQuizCount] = useState(0)
   const [gameIsOver, setGameIsOver] = useState(false)
   const [data, setData] = useState([])
+  const [timeOut, setTimeOut] = useState(false)
 
   useEffect(() => {
-
     goToResults()
     getquizApi()
       .then((res) => {
@@ -35,28 +35,40 @@ function App() {
     }
   }
 
-  function selectHandler(e) {
-    if (e.target.value == e.target.name) {
-      console.log('CORRECT!')
-      console.log('game over? ', gameIsOver)
-      incrementCount()
-      setTimeout(()=>{
-        setTurnsCount(turnsCount + 1)
-      },1000)
-    } else {
-      console.log('ARE YOU SURE!')
-      // setTurnsCount(turnsCount + 1)
-      setTimeout(()=>{
-        setTurnsCount(turnsCount + 1)
-      },600)
+  function outOfTime() {
+    console.log(timeOut)
+    if (timeOut) {
+      setTurnsCount(turnsCount + 1)
+      setTimeOut(false)
+    }
+    if (turnsCount == 5) {
+      setTurnsCount(turnsCount)
     }
   }
+  // outOfTime()
 
+  function selectHandler(e) {
+    if (e.target.value == e.target.name) {
+      e.target.style.backgroundColor = 'green'
+
+      incrementCount()
+      setTimeout(() => {
+        setTurnsCount(turnsCount + 1)
+      }, 1000)
+    } else {
+      e.target.style.backgroundColor = 'red'
+      console.log('ARE YOU SURE!')
+      // setTurnsCount(turnsCount + 1)
+      setTimeout(() => {
+        setTurnsCount(turnsCount + 1)
+      }, 600)
+    }
+  }
 
   const game = (
     <>
       {data.map((quiz) => (
-        <main  key={quiz.id}>
+        <main key={quiz.id}>
           <h3>{quiz.question}</h3>
           {quiz.answers.sort().map((answer) => {
             return (
@@ -72,23 +84,25 @@ function App() {
           })}
         </main>
       ))}
+      {outOfTime()}
     </>
   )
 
   const gameover = (
     <>
       <h1>GAMEOVER!!</h1>
-      <h1>You scored {quizCount}/{turnsCount}!</h1>
+      <h1>
+        You scored {quizCount}/{turnsCount}!
+      </h1>
       <a href="/">Play again?</a>
     </>
   )
-
-
 
   return (
     <>
       <div className="app">
         <h1>Are you sure?!</h1>
+        <Timer turnsCount={turnsCount} setTimeOut={setTimeOut} />
         {gameIsOver ? gameover : game}
       </div>
 
